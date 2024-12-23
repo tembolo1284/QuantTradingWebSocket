4ff66922c6e80a53f6dd385dc701dd3c15b6edfb
Paul Lopez <nik.lopez381@gmail.com>
2024-12-23 10:17:35 -0500

    Updated config file for unity and updated tests.

---
+++ build/tests/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /root/c-workspace/QuantTradingWebSocket/tests
+# Build directory: /root/c-workspace/QuantTradingWebSocket/build/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test_order "/root/c-workspace/QuantTradingWebSocket/build/tests/test_order")
+set_tests_properties(test_order PROPERTIES  _BACKTRACE_TRIPLES "/root/c-workspace/QuantTradingWebSocket/tests/CMakeLists.txt;25;add_test;/root/c-workspace/QuantTradingWebSocket/tests/CMakeLists.txt;0;")
+add_test(test_order_book "/root/c-workspace/QuantTradingWebSocket/build/tests/test_order_book")
+set_tests_properties(test_order_book PROPERTIES  _BACKTRACE_TRIPLES "/root/c-workspace/QuantTradingWebSocket/tests/CMakeLists.txt;26;add_test;/root/c-workspace/QuantTradingWebSocket/tests/CMakeLists.txt;0;")
